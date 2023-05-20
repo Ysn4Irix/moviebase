a2742ec3fc56e709d59aac9fdd4c1bbe5db0a81c
Ysn4Irix <80527916+Ysn4Irix@users.noreply.github.com>
2023-05-20 14:38:16 +0100

    :fire: updating

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '0.0.0.0',
+    baseURL: process.env.DEP_URL,
   },
   serverMiddleware: [`~/serverMiddleware/moviebase`],
   // Build Configuration: https://go.nuxtjs.dev/config-build
